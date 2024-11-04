17b4451b0651b28c77fd5e881330420a27bc46cc
Chun Lau <158354108+j02355151@users.noreply.github.com>
2024-11-04 12:53:26 -0500

    Add files via upload

---
+++ Exam 2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+Assume_speed = 75
+
+If;
+(speed >35);
+
+{
+
+                        Fee = 20;
+
+}
+
+        else
+
+                      {                                   // open else
+
+                      if  (speed > 55) {
+
+                                     Fee = 50; }
+
+                             else{                           // open second else
+
+                                    if  (speed >75) {
+
+                                            Fee =100; }
+
+                                     }                           // close second else
+
+                }                                               // close first else
